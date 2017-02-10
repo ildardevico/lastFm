2afbf37334315310f7ec886e407996191709a8a7
Ildar <ildarka008@gmail.com>
2017-02-10 14:30:17 +0200

    Added config

---
+++ config/index.js
@@ -0,0 +1,14 @@
+export default {
+  host: 'localhost',
+  port: 3000,
+  hotLoader: {
+    host: 'localhost',
+    port: 3001
+  },
+  endpoint: 'http://ws.audioscrobbler.com/2.0/',
+  apiKey: 'ddb4a78e793fff45aa5afe29059605bc',
+  secret: '0b5872ce03567722fe719da5620b4ea5',
+  cb: 'http://localhost:3001/complete-auth',
+  checkAuthUri: 'http://www.last.fm/api/auth/',
+  soundCloudClient: 'f4323c6f7c0cd73d2d786a2b1cdae80c'
+}
